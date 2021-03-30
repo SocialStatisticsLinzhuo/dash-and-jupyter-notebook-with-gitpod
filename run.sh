1b466d0edf5682bf1c31a2a3839716e3d653ebf1
SocialStatisticsLinzhuo <linzhuoli@zju.edu.cn>
2021-03-30 03:07:34 +0000

    4th

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 pip3 install dash # ==0.43.0 The core dash backend
 pip3 install dash-daq # ==0.1.0 DAQ components (newly open-sourced!)
 pip3 install jupyter pandas matplotlib scipy seaborn jupyterlab  # install jupyter notebook, pandas(and dependant numpy), matplotlib
+pip3 install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
 
